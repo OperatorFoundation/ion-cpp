1950c47828169df928e9026d97f54c7882b98668
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2025-10-02 11:55:55 -0500

    Added ion-cpp.h to make Arduino library dependencies work better

---
+++ libraries/ion-cpp/src/ion-cpp.h
@@ -0,0 +1,18 @@
+//
+// Created by Dr. Brandon Wiley on 10/2/25.
+//
+
+#ifndef ION_CPP_ION_CPP_H
+#define ION_CPP_ION_CPP_H
+
+#include "types.h"
+#include "StorageConnection.h"
+#include "storage/storage.h"
+#include "storage/word.h"
+#include "storage/ion_float.h"
+#include "storage/word_array.h"
+#include "storage/float_array.h"
+#include "storage/mixed_array.h"
+#include "squeeze.h"
+
+#endif //ION_CPP_ION_CPP_H
